fab7aeed2c2422d36634c1fdc7e82167a7cd11d4
TedEngineer <tedvu184@gmail.com>
2022-09-04 12:34:02 +1000

    Add custom types for graphql server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,24 +3,32 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
-        name: String!
+      me: User!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
     }
 `;
 
 // Resolvers what would be returned when hitting a particular query
 const resolvers = {
   Query: {
-    hello() {
-      return "Hello World!";
-    },
-    name() {
-      return "My name is Ted";
+    me() {
+      return {
+        id: "12398",
+        name: "Ted Vu",
+        email: "tedvu184@gmail.com",
+        age: 24,
+      };
     },
   },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => {
-  console.log("The server is up");
+  console.log("The server is up on port 4000");
 });
